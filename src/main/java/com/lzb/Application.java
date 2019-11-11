4dfc1f209d2ada579d995722a736b3d138b6a87f
lzb <1255863672@qq.com>
2019-11-11 21:31:54 +0800

    修改了拦截器代码、撤销了之前的代码

--- src/main/java/com/lzb/Application.java
+++ src/main/java/com/lzb/Application.java
@@ -17,15 +17,4 @@ public class Application {
         System.out.println("你好");
     }
 }
-//皮卡丘看着你
-//1421521
-//215151511252115
-//21512512521521
-//215215215
-    //是的撒多撒555555555
-    //5ds4d5sa4878979879
-    //
-// 惹我热无若
-//21512512521521wfewfewfwefew
-//215215215wfeewfiwpjpwfjwf
-//amngoieniewpnvpivnpivnpewvnpiwniewnvnepvinipwe
+//撤销了所有代码修改
--- src/main/java/com/lzb/interceptor/MyInterceptor.java
+++ src/main/java/com/lzb/interceptor/MyInterceptor.java
@@ -21,9 +21,11 @@ public class MyInterceptor implements HandlerInterceptor {
     }
 
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        System.out.println("拦截器：postHandle-----");
     }
 
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        System.out.println("拦截器：afterCompletion");
     }
 
 }
