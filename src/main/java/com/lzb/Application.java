f3af27e0650a775c698c3cbfacbcee0c3b7ec87c
lzb <1255863672@qq.com>
2019-11-11 21:04:19 +0800

    修改Application

--- src/main/java/com/lzb/Application.java
+++ src/main/java/com/lzb/Application.java
@@ -11,8 +11,15 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan("com.lzb.mapper")   //mapper接口扫描：在main方法上添加@MapperScan("包名") 注解
 public class Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+        System.out.println("你好");
     }
 }
-//皮卡丘看着你
+//皮卡丘1111
+    //是的撒多撒
+    //5ds4d5sa4
+    //555
+//52153165
+//4444
