e388517e9717a2808143de5ec6bebaab1ab051fe
LiangXiaochen <liangxiaochen_lxc@163.com>
2019-11-11 21:19:16 +0800

    Update Application.java

--- src/main/java/com/lzb/Application.java
+++ src/main/java/com/lzb/Application.java
@@ -20,5 +20,6 @@ public class Application {
 //皮卡丘看着你
 //1421521
 //215151511252115
-//21512512521521
-//215215215
+//21512512521521wfewfewfwefew
+//215215215wfeewfiwpjpwfjwf
+//amngoieniewpnvpivnpivnpewvnpiwniewnvnepvinipwe
